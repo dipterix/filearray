9e92830b897b6334e3092b7164c4d1eda8f8bc23
dipterix <dipterix.wang@gmail.com>
2023-04-12 23:56:24 -0400

    use universal pairwise operators

--- R/methods-add.R
+++ R/methods-add.R
@@ -1,79 +1,10 @@
 
-fa_add_internal <- function(e1, e2, label = "add array") {
-    
-    stopifnot(is_filearray(e1) || is_filearray(e2))
-    out_type <- operation_output_type(typeof(e1), typeof(e2))
-    
-    # check if e1 is scalar
-    if(length(e1) == 1) {
-        e2 <- as_filearrayproxy(e2)
-        uuid2 <- e2$uuid()
-        op_func <- function(v, ...) {
-            # v is an env with names: uuids
-            return(e1 + v[[ uuid2 ]])
-        }
-        e2$add_operator( op_func, out_type = out_type, context = "scalar", label = label )
-        return(e2)
-    }
-    
-    # check if e2 is a scalar
-    if(length(e2) == 1) {
-        e1 <- as_filearrayproxy(e1)
-        uuid1 <- e1$uuid()
-        op_func <- function(v, ...) {
-            # v is an env with names: uuids
-            return(v[[ uuid1 ]] + e2)
-        }
-        e1$add_operator( op_func, out_type = out_type, context = "scalar", label = label )
-        return(e1)
-    }
-    
-    # check if e1 or e2 is numerical
-    if(!is_filearray(e1)) {
-        if(!is_same_dim(e1, e2)) {
-            stop("non-conformable arrays")
-        }
-        e1 <- as_filearray(e1, dimension = dim(e2))
-    }
-    
-    if(!is_filearray(e2)) {
-        if(!is_same_dim(e2, e1)) {
-            stop("non-conformable arrays")
-        }
-        e2 <- as_filearray(e2, dimension = dim(e1))
-    }
-    
-    # e1 and e2 must be filearray or filearray proxy
-    if(!is_same_dim(e1, e2)) {
-        stop("non-conformable arrays")
-    }
-    
-    e1 <- as_filearrayproxy(e1)
-    e2 <- as_filearrayproxy(e2)
-    
-    
-    uuid1 <- e1$uuid()
-    uuid2 <- e2$uuid()
-    e1$link_proxy( e2 )
-    
-    # check input types 
-    out_type <- operation_output_type(typeof(e1), typeof(e2))
-    
-    op_func <- function(v, ...) {
-        return(v[[ uuid1 ]] + v[[ uuid2 ]])
-    }
-    
-    e1$add_operator( op_func, out_type = out_type, context = "array", label = label )
-    
-    return( e1 )
-}
-
 fa_add <- function(e1, e2) {
     call <- match.call()
     call[[1]] <- quote(`+`)
     label <- sprintf("Calculating: %s (fa_add)", deparse1(call))
     
-    fa_add_internal(e1, e2, label)
+    fa_pairwise_operator(e1, e2, op = "+", label = label)
 }
 
 
